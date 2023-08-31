882208f551fffc018d1c6ce9f8a9e4b54f545008
NamohS <112482434+NamohS@users.noreply.github.com>
2023-08-31 12:04:34 +0530

    Add files via upload

---
+++ func1.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+using namespace std;
+int sum(int n,int m)
+{return n+m;}
+double sum(double n,double m)
+{return n+m;}
+double sum(double n,int m)
+{return n+m;}
+double sum(int n,double m)
+{return n+m;}
+
+int sum(int a,int b,int c)
+{return a+b+c;}
+double sum(int a , int b,double c)
+{return a+b+c;}
+double sum(int a , double b,int c)
+{return a+b+c;}
+double sum(int a , double b,double c)
+{return a+b+c;}
+double sum(double a,int b,int c)
+{return a+b+c;}
+double sum(double a,int b,double c)
+{return a+b+c;}
+double sum(double a,double b,int c)
+{return a+b+c;}
+double sum(double a,double b,double c)
+{return a+b+c;}
+int sum(int a, int b, int c, int d) {
+  return a + b + c + d;
+}
+
+double sum(int a, int b, int c, double d) {
+  return a + b + c + d;
+}
+
+double sum(int a, int b, double c, int d) {
+  return a + b + c + d;
+}
+
+double sum(int a, int b, double c, double d) {
+  return a + b + c + d;
+}
+
+double sum(int a, double b, int c, int d) {
+  return a + b + c + d;
+}
+
+double sum(int a, double b, int c, double d) {
+  return a + b + c + d;
+}
+
+double sum(int a, double b, double c, int d) {
+  return a + b + c + d;
+}
+
+double sum(int a, double b, double c, double d) {
+  return a + b + c + d;
+}
+
+double sum(double a, int b, int c, int d) {
+  return a + b + c + d;
+}
+
+double sum(double a, int b, int c, double d) {
+  return a + b + c + d;
+}
+
+double sum(double a, int b, double c, int d) {
+  return a + b + c + d;
+}
+
+double sum(double a, int b, double c, double d) {
+  return a + b + c + d;
+}
+
+double sum(double a, double b, int c, int d) {
+  return a + b + c + d;
+}
+
+double sum(double a, double b, int c, double d) {
+  return a + b + c + d;
+}
+
+double sum(double a, double b, double c, int d) {
+  return a + b + c + d;
+}
+
+double sum(double a, double b, double c, double d) {
+  return a + b + c + d;
+}
+int main()
+{
+    cout<<sum(5,6)<<endl;
+    cout<<sum(5.9,6.8)<<endl;
+    cout<<sum(5.7,6)<<endl;
+    cout<<sum(5,6.3)<<endl;
+
+    cout<<sum(5,6,7)<<endl;
+    cout<<sum(5,6,7.5)<<endl;
+    cout<<sum(5,7.6,6)<<endl;
+    cout<<sum(5,6.8,3.7)<<endl;
+    cout<<sum(5.7,6,7)<<endl;
+    cout<<sum(5.7,6,7.8)<<endl;
+    cout<<sum(5.7,6.7,7)<<endl;
+    cout<<sum(5.7,6.7,7.8)<<endl;
+
+    cout<<sum(5,6,7,8)<<endl;
+    cout<<sum(5,6,7,8.8)<<endl;
+    cout<<sum(5,6,7.8,8)<<endl;
+    cout<<sum(5,6,7.5,8.8)<<endl;
+    cout<<sum(5,6.4,7,8)<<endl;
+    cout<<sum(5,6.7,7,8.8)<<endl;
+    cout<<sum(5,6.6,7.8,8)<<endl;
+    cout<<sum(5,6.6,7.8,8.9)<<endl;
+    cout<<sum(5.1,6,7,8)<<endl;
+    cout<<sum(5.1,6,7,8.7)<<endl;
+    cout<<sum(5.2,6,7.3,8)<<endl;
+    cout<<sum(5.7,6,7.7,6.9)<<endl;
+    cout<<sum(5.5,6.5,7,8)<<endl;
+    cout<<sum(5.4,6.3,7,8.8)<<endl;
+    cout<<sum(5.2,6.7,6.7,8)<<endl;
+    cout<<sum(5.5,6.4,7.8,8.9)<<endl;
+    
+
+
+    
+
+
+
+
+
+
+    
+}
